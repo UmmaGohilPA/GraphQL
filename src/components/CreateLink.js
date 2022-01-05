e92e78a4768702cbc5e5983164f695e996287db0
umma-pa <umma.gohil@paconsulting.com>
2022-01-05 15:38:03 +0000

    More mutations/updating store

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
+import { FEED_QUERY } from "./LinkList";
+import { LINKS_PER_PAGE } from "../components/constants";
 
 const CREATE_LINK_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -24,7 +26,35 @@ const CreateLink = () => {
       description: formState.description,
       url: formState.url,
     },
-    onCompleted: () => navigate("/"),
+    update: (cache, { data: { post } }) => {
+      const take = LINKS_PER_PAGE;
+      const skip = 0;
+      const orderBy = { createdAt: "desc" };
+
+      const data = cache.readQuery({
+        query: FEED_QUERY,
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: [post, ...data.feed.links],
+          },
+        },
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
+    },
+    onCompleted: () => History.push("/new/1"),
   });
   return (
     <div>
